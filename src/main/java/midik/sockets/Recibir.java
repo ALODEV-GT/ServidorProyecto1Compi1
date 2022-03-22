8c0ea7d40133bd539559ae1d775d345ed3e18f2e
Brayan Alonzo <brayan.quialo@gmail.com>
2022-03-22 13:55:01 -0600

    Se agrego la funcionalidad para recibir los proyectos

---
+++ src/main/java/midik/sockets/Recibir.java
@@ -0,0 +1,53 @@
+package midik.sockets;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Recibir {
+
+    private final DataInputStream in;
+
+    public Recibir(DataInputStream in) {
+        this.in = in;
+    }
+
+    public void recibir() throws IOException {
+        crearDirectorios();
+        int cantidadArchivosP1 = this.in.readInt();
+        int cantidadArchivosP2 = this.in.readInt();
+
+        for (int i = 0; i < cantidadArchivosP1; i++) {
+            recibirArchivo("Proyecto1");
+        }
+
+        for (int i = 0; i < cantidadArchivosP2; i++) {
+            recibirArchivo("Proyecto2");
+        }
+    }
+
+    private void recibirArchivo(String nombreDirectorio) throws IOException {
+        String nombreArchivo = in.readUTF();
+        int tamanioArchivo = in.readInt();
+        byte[] contenidoArchivo = new byte[tamanioArchivo];
+        for (int j = 0; j < tamanioArchivo; j++) {
+            contenidoArchivo[j] = in.readByte();
+        }
+        String contenido = new String(contenidoArchivo);
+        FileWriter fw = new FileWriter(nombreDirectorio + "/" + nombreArchivo);
+        fw.write(contenido);
+        fw.close();
+    }
+
+    private void crearDirectorios() {
+        File directorioP1 = new File("Proyecto1");
+        File directorioP2 = new File("Proyecto2");
+        if (!directorioP1.exists()) {
+            directorioP1.mkdir();
+        }
+        if (!directorioP2.exists()) {
+            directorioP2.mkdir();
+        }
+    }
+}
---
+++ src/main/java/midik/sockets/Servidor.java
@@ -0,0 +1,39 @@
+package midik.sockets;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor {
+
+    public static void main(String[] args) {
+        ServerSocket servidor = null;
+        Socket sc = null;
+        DataInputStream in;
+        DataOutputStream out;
+        final int PUERTO = 5000;
+
+        try {
+            servidor = new ServerSocket(PUERTO);
+            System.out.println("Servidor iniciado.");
+
+            while (true) {
+                sc = servidor.accept(); //A la espera de un cliente
+                System.out.println("Cliente conectado.");
+
+                in = new DataInputStream(sc.getInputStream());
+                out = new DataOutputStream(sc.getOutputStream());
+
+                Recibir recibir = new Recibir(in);
+                recibir.recibir();
+
+                sc.close();
+                System.out.println("Cliente desconectado");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+}
